24e22c3de2e8ee4a04f4e455f5ab4aa2e93483c7
huangxz <huangxz098@163.com>
2017-05-04 11:38:08 +0800

    new

---
+++ js/issue.js
@@ -0,0 +1,20 @@
+/*
+* @Author: huangxz
+* @Date:   2017-05-04 10:48:25
+* @Last Modified by:   huangxz
+* @Last Modified time: 2017-05-04 10:51:01
+*/
+
+'use strict';
+$(function(){
+
+ $('.icon-squre-plus').click(function(){
+
+ 	$('.1').show();
+ })
+
+
+
+
+
+});
